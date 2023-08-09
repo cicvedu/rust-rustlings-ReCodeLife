cb10d09ff730d70d558049212b431072a93e4b4a
ReCodeLife <recodelife@proton.me>
2023-08-09 16:28:56 +0000

    threads

--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -3,13 +3,12 @@
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::sync::mpsc;
 use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
-
+use std::sync::Mutex;
 struct Queue {
     length: u32,
     first_half: Vec<u32>,
@@ -30,11 +29,14 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     let qc = Arc::new(q);
     let qc1 = Arc::clone(&qc);
     let qc2 = Arc::clone(&qc);
+    let tx1=Arc::new(Mutex::new(tx));
+    let tx2=Arc::clone(&tx1);
+
 
     thread::spawn(move || {
         for val in &qc1.first_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx1.lock().unwrap().send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
@@ -42,7 +44,7 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     thread::spawn(move || {
         for val in &qc2.second_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx2.lock().unwrap().send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
